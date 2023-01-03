8414bed4486cc77e132e5f5422db85a72231ab7f
Jose Antonio GV <jantoniogv85@gmail.com>
2023-01-03 21:20:02 +0100

    Añadir temporizador arranque motor

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
 #include "display_oled.h"
 #include "serial_rx.h"
 #include "device.h"
+#include "device_serial_handler.h"
 #include "current_measurement.h"
 
 #include "debug_utils.h"
@@ -28,13 +29,12 @@ void setup()
   // Inicia la conexion serial
   serial_init();
 
-  Serial.println("casa");
-  DEBUG_PRINT("lugar");
+  DEBUG_PRINT("Serial iniciado...");
 
   // Inicia el modulo LoRa
   init_lora();
 
-  DEBUG_PRINT("lora");
+  DEBUG_PRINT("LoRa iniciado...");
 
   // Inicia el display OLED
   // init_oled();
@@ -75,6 +75,9 @@ void setup()
 
   // Inicia la tarea que recibe los datos por LoRa
   xTaskCreatePinnedToCore(receiveDataLora, "loraData", 2048, nullptr, 5, nullptr, 1);
+
+  // Iniciamos el temporizador encargado de medir la corriente del motor del pozo periodicamente
+  start_motor_timer = xTimerCreate("start_motor_timer", pdMS_TO_TICKS(17000), pdFALSE, (void *)0, reinterpret_cast<TimerCallbackFunction_t>(start_motor));
 
   // Iniciamos el temporizador encargado de medir la corriente del motor del pozo periodicamente
   current_measure_timer = xTimerCreate("current_measure", pdMS_TO_TICKS(5000), pdTRUE, (void *)0, reinterpret_cast<TimerCallbackFunction_t>(current_measure));
